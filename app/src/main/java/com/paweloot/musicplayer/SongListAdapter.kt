0605927519c3165b573431d5063fec969b145285
Paweł Banaś <paweloot@gmail.com>
2019-11-26 23:06:37 +0100

    Rewrite SongHolder and onClick

--- app/src/main/java/com/paweloot/musicplayer/SongListAdapter.kt
+++ app/src/main/java/com/paweloot/musicplayer/SongListAdapter.kt
@@ -1,12 +1,10 @@
 package com.paweloot.musicplayer
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-
-
+import androidx.recyclerview.widget.RecyclerView
 import com.paweloot.musicplayer.SongListFragment.OnSongSelectedListener
 
 
@@ -14,15 +12,6 @@ class SongListAdapter(
     private val songs: List<Song>,
     private val onSongSelectedListener: OnSongSelectedListener
 ) : RecyclerView.Adapter<SongListAdapter.SongHolder>() {
-
-    private val onClickListener: View.OnClickListener
-
-    init {
-        onClickListener = View.OnClickListener { v ->
-            val song = v.tag as Song
-            onSongSelectedListener.onSongSelected(song)
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongHolder {
         val view = LayoutInflater.from(parent.context)
@@ -33,21 +22,31 @@ class SongListAdapter(
 
     override fun onBindViewHolder(holder: SongHolder, position: Int) {
         val song = songs[position]
-
-        holder.titleTextView.text = song.title
-        holder.artistTextView.text = song.artist
-
-        with(holder.view) {
-            tag = song
-            setOnClickListener(onClickListener)
-        }
+        holder.bind(song)
     }
 
     override fun getItemCount(): Int = songs.size
 
-    inner class SongHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView = view.findViewById(R.id.title)
-        val artistTextView: TextView = view.findViewById(R.id.artist)
+    inner class SongHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
+        private val titleTextView: TextView = view.findViewById(R.id.title)
+        private val artistTextView: TextView = view.findViewById(R.id.artist)
 
+        private lateinit var song: Song
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        fun bind(song: Song) {
+            this.song = song
+
+            titleTextView.text = song.title
+            artistTextView.text = song.artist
+        }
+
+        override fun onClick(v: View) {
+            onSongSelectedListener.onSongSelected(song)
+        }
     }
 }
