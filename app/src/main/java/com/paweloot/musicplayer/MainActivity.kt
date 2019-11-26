a0556a3fc3ad6b8f93e1e37a508ac24fcf3e1619
Paweł Banaś <paweloot@gmail.com>
2019-11-26 22:39:01 +0100

    Remove DummyContent

--- app/src/main/java/com/paweloot/musicplayer/MainActivity.kt
+++ app/src/main/java/com/paweloot/musicplayer/MainActivity.kt
@@ -2,7 +2,6 @@ package com.paweloot.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.paweloot.musicplayer.dummy.DummyContent
 
 class MainActivity : AppCompatActivity(), SongListFragment.OnSongSelectedListener {
 
--- app/src/main/java/com/paweloot/musicplayer/SongListFragment.kt
+++ app/src/main/java/com/paweloot/musicplayer/SongListFragment.kt
@@ -9,9 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.paweloot.musicplayer.dummy.DummyContent
-import com.paweloot.musicplayer.dummy.DummyContent.DummyItem
-
 
 class SongListFragment : Fragment() {
 
