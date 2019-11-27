b7fa96c940eb98edb207185a08739ddcf1680e2f
Paweł Banaś <paweloot@gmail.com>
2019-11-27 13:11:45 +0100

    Add SongDataManager as a Singleton

---
+++ app/src/main/java/com/paweloot/musicplayer/SongDataManager.kt
@@ -0,0 +1,60 @@
+package com.paweloot.musicplayer
+
+import android.content.Context
+import android.provider.MediaStore
+
+class SongDataManager private constructor(private val context: Context) {
+
+    val songData: MutableList<Song> = mutableListOf()
+
+    init {
+        querySongData()
+    }
+
+    private fun querySongData() {
+        val projection = arrayOf(
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM_ID
+        )
+        val selection = MediaStore.Audio.Media.IS_MUSIC + " == 1"
+        val sort = MediaStore.Audio.Media.TITLE + " ASC"
+
+        with(context as MainActivity) {
+            val cursor = contentResolver.query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection, selection, null, sort
+            )
+
+            if (cursor != null && cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val data = cursor.getString(0)
+                    val title = cursor.getString(1)
+                    val album = cursor.getString(2)
+                    val artist = cursor.getString(3)
+                    val albumId = cursor.getString(4)
+
+                    songData.add(Song(data, title, artist, album, albumId))
+                }
+            }
+
+            cursor?.close()
+        }
+    }
+
+    companion object {
+        private var INSTANCE: SongDataManager? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = SongDataManager(context)
+            }
+        }
+
+        fun get(): SongDataManager {
+            return INSTANCE ?: throw IllegalStateException("SongDataManager must be initialized!")
+        }
+    }
+}
