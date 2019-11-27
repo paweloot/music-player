885e7a11b85b79d58c08d9bc36db0de9a5114871
Paweł Banaś <paweloot@gmail.com>
2019-11-27 22:28:02 +0100

    Update current song data using MainActivity ViewModel

--- app/src/main/java/com/paweloot/musicplayer/CurrentSongFragment.kt
+++ app/src/main/java/com/paweloot/musicplayer/CurrentSongFragment.kt
@@ -4,16 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 
 class CurrentSongFragment : Fragment() {
+
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+
+    private lateinit var titleTextView: TextView
+    private lateinit var artistTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainActivityViewModel = ViewModelProviders.of(requireActivity())
+            .get(MainActivityViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_current_song, container, false)
+        val view =
+            inflater.inflate(R.layout.fragment_current_song, container, false)
+
+        titleTextView = view.findViewById(R.id.song_title)
+        artistTextView = view.findViewById(R.id.song_artist)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainActivityViewModel.currentSong.observe(
+            viewLifecycleOwner,
+            Observer { song ->
+                titleTextView.text = song.title
+                artistTextView.text = song.artist
+            }
+        )
     }
 
     companion object {
