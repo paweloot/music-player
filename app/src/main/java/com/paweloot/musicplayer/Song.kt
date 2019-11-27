f451f3fa782e34c279c4d15d513e8aa2e5d23077
Paweł Banaś <paweloot@gmail.com>
2019-11-27 13:24:03 +0100

    Add more data to hold

--- app/src/main/java/com/paweloot/musicplayer/Song.kt
+++ app/src/main/java/com/paweloot/musicplayer/Song.kt
@@ -1,8 +1,11 @@
 package com.paweloot.musicplayer
 
 data class Song(
+    val dataPath: String,
     val title: String,
-    val artist: String
+    val artist: String,
+    val album: String,
+    val albumId: String
 ) {
 
 }
